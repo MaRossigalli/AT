f2fec547d07ad2da145da1852670b51ffaeeb8c5
MaRossigalli <74995315+MaRossigalli@users.noreply.github.com>
2020-11-24 21:02:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+anime({
+    targets: '.anime',
+    opacity: [0, 1],
+    translateX: [-120, 0],
+    delay: anime.stagger(100, { from: 'first', start: 100 }),
+    easing: 'easeOutExpo',
+    // duration: 30000
+})
